ce353e363a0cbf80e5f4e8c0c28ad1a230e5e682
Cavell Blood <cblood@farmersfriendllc.com>
2019-06-19 17:23:04 -0500

    remove cp asset dependencies

--- src/assetbundles/sunsettosunset/SunsetToSunsetAsset.php
+++ src/assetbundles/sunsettosunset/SunsetToSunsetAsset.php
@@ -45,10 +45,10 @@ class SunsetToSunsetAsset extends AssetBundle
         // define the path that your publishable resources live
         $this->sourcePath = "@cavellblood/sunsettosunset/assetbundles/sunsettosunset/dist";
 
-        // define the dependencies
-        $this->depends = [
-            CpAsset::class,
-        ];
+//        // define the dependencies
+//        $this->depends = [
+//            CpAsset::class,
+//        ];
 
         // define the relative path to CSS/JS files that should be registered with the page
         // when this asset bundle is registered
